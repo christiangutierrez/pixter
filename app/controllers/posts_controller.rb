4c58b551ca0a1c9c353806b21d299b4691a92cfa
Christian Gutierrez <christian@amberbluefilms.com>
2014-04-19 22:06:49 +0000

    Add Posts Feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,24 @@
 class PostsController < ApplicationController
   def index
     
-    @posts = Post.all
-    
-      
+    @posts = Post.all 
   end
   
+  def new
+    @post = Post.new
+  end
+  
+  #This is the code that gets run when the create post button is pushed and then redirect them to the posts. 
+  def create
+    Post.create( post_params )
+    redirect_to posts_path 
+  end
+  
+  #Everything underneath this private, is not a regular page.   
+  private
+    
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+    
 end
